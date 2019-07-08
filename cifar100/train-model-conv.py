4482ec7e6f0657ee91742087e742d959d623f0ee
Matty <matty.hempstead@gmail.com>
2019-07-08 19:32:32 +1000

    Slightly improve cifar100 to 51% accuracy

--- cifar100/train-model-conv.py
+++ cifar100/train-model-conv.py
@@ -38,57 +38,51 @@ testImages = testImages / 255
 # print(trainImages[0])
 
 
+act = tf.keras.activations.elu
 
-def getConvModel():
+def getModel():
     model = keras.Sequential()
 
-    model.add(keras.layers.Conv2D(32, (3,3), activation=tf.nn.relu, input_shape=(32,32,3), strides=(1,1)))
-    model.add(keras.layers.Dropout(0.2, seed=0))
 
-    model.add(keras.layers.Conv2D(64, (3,3), activation=tf.nn.relu, strides=(2,2)))
-    model.add(keras.layers.Dropout(0.35, seed=0))
-
-    model.add(keras.layers.MaxPooling2D((2, 2)))
-
-    model.add(keras.layers.Conv2D(128, (3,3), activation=tf.nn.relu, strides=(2,2)))
-    model.add(keras.layers.Dropout(0.35, seed=0))
-
-    model.add(keras.layers.Flatten())
-
-    model.add(keras.layers.Dense(512, activation=tf.nn.relu))
-    model.add(keras.layers.Dropout(0.35, seed=0))
-
-    model.add(keras.layers.Dense(256, activation=tf.nn.relu))
-    model.add(keras.layers.Dropout(0.35, seed=0))
-
-    model.add(keras.layers.Dense(100, activation=tf.nn.softmax))
-
-    return model
-
-
-def getOtherModel():
-    model = keras.Sequential()
-    model.add(keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+    model.add(keras.layers.Conv2D(32, (3, 3), activation=act, input_shape=(32, 32, 3)))
     # model.add(keras.layers.Dropout(0.05))
-    model.add(keras.layers.Conv2D(32, (3, 3), activation='relu'))
+
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Conv2D(32, (3, 3), activation=act))
     model.add(keras.layers.MaxPooling2D((2, 2)))
     model.add(keras.layers.Dropout(0.3))
-    model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
-    model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
+
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Conv2D(64, (3, 3), activation=act))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Conv2D(64, (3, 3), activation=act))
     model.add(keras.layers.MaxPooling2D((2, 2)))
     model.add(keras.layers.Dropout(0.3))
-    model.add(keras.layers.Conv2D(128, (3, 3), activation='relu'))
-    model.add(keras.layers.Conv2D(128, (3, 3), activation='relu'))
+
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Conv2D(128, (3, 3), activation=act))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Conv2D(128, (3, 3), activation=act))
     # model.add(keras.layers.MaxPooling2D((2, 2)))
     model.add(keras.layers.Dropout(0.3))
     model.add(keras.layers.Flatten())
-    model.add(keras.layers.Dense(128, activation='relu'))
+
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Dense(256, activation=act))
     model.add(keras.layers.Dropout(0.3))
+
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Dense(128, activation=act))
+    model.add(keras.layers.Dropout(0.3))
+
+    model.add(keras.layers.BatchNormalization())
     model.add(keras.layers.Dense(100, activation='softmax'))
     return model
 
 
-model = getOtherModel()
+
+
+model = getModel()
 
 
 model.compile(
@@ -107,7 +101,7 @@ model.summary()
 
 
 
-epochs = 100
+epochs = 50
 
 
 trainResults = model.fit(
