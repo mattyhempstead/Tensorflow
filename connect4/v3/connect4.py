697168c074ca3555790c13036a52a8bb1913310e
Matty <matty.hempstead@gmail.com>
2019-07-14 17:50:36 +1000

    Add actual connect4 q-learning
    Doesn't work very well yet.

---
+++ connect4/v3/connect4.py
@@ -0,0 +1,372 @@
+import sys, math, random, numpy as np
+import selectMove
+
+class Connect4Game():
+    def __init__(self):
+
+        self.turn = 1
+        self.turnNum = 0	# Keeps track of number of turns played (useful for detecting game over)
+
+        self.board = np.zeros((7,6), dtype="int8")
+
+    def __repr__(self):
+        '''
+            Returns a string repr of the board state
+        '''
+        board = np.array([[j for j in i][::-1] for i in self.board])
+        board = np.transpose(board)
+
+        string = ""
+        string += "\n"
+        for row in board:
+            for col in row:
+                string += " X " if col==1 else (" O " if col==-1 else " - ")
+            string += "\n"
+        string += " 1  2  3  4  5  6  7 "
+        string += "\n"
+        return string
+
+    def printGame(self):
+        '''
+            Prints a basic version of the board state to stdout
+        '''
+        print(self)
+
+    def move(self, col):	
+        '''
+            Makes move in a particular column and switches turn \n
+            If move ends game, winner is returned (1 or 0 or -1) \n
+            Otherwise returns None \n
+            If move is not valid, prints to stdout \n
+        '''
+        
+        for row in range(6):
+            if self.board[col][row] == 0:
+
+                self.board[col][row] = self.turn
+                self.turnNum += 1			
+
+                winner = self.doesMoveEndGame(col, row, self.turn)
+
+                if winner == None:
+                    self.turn *= -1
+                
+                return winner
+
+        print("Invalid move")
+
+    def placePiece(self, col, row):
+        self.board[col][row] = self.turn
+
+    def removePiece(self, col, row):
+        self.board[col][row] = 0
+
+    def placeCol(self, col):
+        row = self.getValidMove(col)
+        self.placePiece(col,row)
+    
+    def removeCol(self, col):
+        for i in range(5,-1,-1):
+            if self.board[col][i] != 0:
+                self.board[col][i] = 0
+                return
+
+    def doesMoveEndGame(self, col, row, turn):	
+        ''' 
+            Returns whether moving in a particular position ends the game \n
+            Returns 1, 0, or -1 if this particular move ends the game, otherwise return None
+        '''
+
+        # Check whether piece creates vertical 4 in a row
+        count = 1
+        for rowBelow in range(row-1, max(row-4,-1), -1):
+            if self.board[col][rowBelow] == turn:
+                count += 1
+                if count == 4:
+                    return turn
+        
+        
+        # Check if piece creates horizontal 4 in a row
+        count = 1
+
+        for colRight in range(col+1, min(col+4,7)):
+            if self.board[colRight][row] == turn:
+                count += 1
+            else:
+                break	
+
+        for colLeft in range(col-1, max(col-4,-1), -1):
+            if self.board[colLeft][row] == turn:
+                count += 1
+            else:
+                break
+
+        if count >= 4:
+            return turn
+
+
+        # Check whether piece create diagonal TL to BR
+        count = 1
+
+        for offset in range(1, 4):		# Bottom right of piece
+            if col+offset==7 or row-offset==-1 or self.board[col+offset][row-offset] != turn:
+                break
+            count += 1
+
+        for offset in range(1, 4):	# Top left of piece
+            if col-offset==-1 or row+offset==6 or self.board[col-offset][row+offset] != turn:
+                break
+            count += 1
+
+        if count >= 4:
+            return turn
+
+
+        # Check whether piece create diagonal TR to BL
+        count = 1
+
+        for offset in range(1, 4):		# Bottom left of piece
+            if col-offset==-1 or row-offset==-1 or self.board[col-offset][row-offset] != turn:
+                break
+            count += 1
+
+        for offset in range(1, 4):	# Top right of piece
+            if col+offset==7 or row+offset==6 or self.board[col+offset][row+offset] != turn:
+                break
+            count += 1
+
+        if count >= 4:
+            return turn
+
+        # Game is tie since all squares are filled
+        if self.turnNum == 42:
+            return 0
+
+        # Move does NOT end game
+        return None
+
+    def isValidMove(self, col):		# Returns whether a move is valid
+        return self.board[col][5] == 0
+
+    def getValidMove(self, col):
+        ''' 
+            Returns the y index of a move in a particular column \n
+            If no move valid, returns -1
+        '''
+        for row in range(6):
+            if self.board[col][row] == 0:
+                return row
+        return -1
+
+    def resetGame(self):
+        self.board.fill(0)
+        self.turn = 1
+        self.turnNum = 0
+
+    def getBoardState(self):
+        ''' Returns a flattened array of board for current player '''
+        return self.board.flatten() * self.turn
+
+    def getInput(self, move):		
+        ''' 
+            Gets the neural net input for the current board state \n
+            Current player always sees their pieces as 1 and opponent as -1 \n
+        '''
+        moveArr = np.zeros(7, dtype="int8")
+        moveArr[move] = 1
+        return np.append(self.getBoardState(), moveArr)
+
+    def getMove(self, model):
+        '''
+            Calculates the move made by an agent in the current game. \n
+            Returns the index of the move, its Q-value, and the s-a input in a 3-tuple. \n
+             - Can be made more efficient by getting .copy's of the same .getInput()?
+        '''
+        validMoves = [i for i in range(7) if (self.isValidMove(i))]
+
+        saArray = []    # Array of all possible state-action pairs as flattened network input arrays
+        for i in validMoves:
+            saArray.append(self.getInput(i))
+        saArray = np.array(saArray)
+        predictions = model.predict(saArray)
+
+        # moveRanks = np.zeros(7, dtype=float)
+        # for i in range(7):
+        #     if i in validMoves:
+        #         moveRanks[i] = predictions[validMoves.index(i)][0]
+
+        # moveArg = predictions.argmax()
+        moveArg = selectMove.maxMoveProb(predictions, 0.9)
+
+        move = validMoves[moveArg]
+        qValue = predictions[moveArg]
+        sa = saArray[moveArg]
+        return {
+            "move": move,
+            "predictions": predictions,
+            "validMoves": validMoves,
+            "qValue": qValue,
+            "sa": sa,
+            "saArray": saArray
+        }
+
+    def getMaxQValue(self, model, sPrime):
+        '''
+            Returns the maximum Q-value for an sPrime
+            If sPrime is 0 or 1, it is a winning state and will return that number
+        '''
+        if type(sPrime) == int:
+            return sPrime
+
+        predictions = model.predict(sPrime)
+
+        moveArg = predictions.argmax()
+
+        qValue = predictions[moveArg][0]
+        return qValue
+
+
+
+
+class RandomAgent():
+    def __init__(self, game):
+        self.game = game
+
+    def getMove(self):
+        validMoves = []
+
+        for i in range(7):		# for all possible moves
+            if self.game.isValidMove(i):
+                validMoves.append(i)
+
+        return random.sample(validMoves, 1)[0]
+
+    def playMove(self):
+        return self.game.move(self.getMove())
+
+
+
+class GoodAgent():
+    def __init__(self, game, winningPlacements=True):
+        self.game = game
+        self.validMoves = np.zeros(7, dtype="int8")
+        self.validMoveCount = 0
+
+        # Whether or not agent will always place in winning placements
+        self.winningPlacements = winningPlacements
+
+    def playMove(self):
+        return self.game.move(self.getMove())
+
+    def getMove(self):
+
+        # Check for winning placements
+        if self.winningPlacements:
+            for col in range(7):
+                # If move has no free slots, skip this column
+                if self.game.board[col][5] != 0:
+                    continue
+
+                # Find row which piece would be placed in this column
+                row = 0
+                for row in range(6):
+                    if self.game.board[col][row] == 0:
+                        break
+
+                winner = self.game.doesMoveEndGame(col, row, self.game.turn)
+
+                if winner == None:
+                    continue
+                return col
+
+
+        # Return a random 'safe' move
+        self.validMoves.fill(0)
+        self.validMoveCount = 0
+
+        
+        # Check for blocking placements
+        for col in range(7):
+            # If move has no free slots, skip this column
+            if self.game.board[col][5] != 0:
+                continue
+
+            # Find row which piece would be placed in this column
+            row = 0
+            for row in range(6):
+                if self.game.board[col][row] == 0:
+                    break
+
+
+            # If piece blocks opponent, always play here
+            winner = self.game.doesMoveEndGame(col, row, -self.game.turn)
+            if winner == -self.game.turn:
+                return col
+
+
+            # Add moves that don't cause opponent to win next turn (safe moves)
+            if row == 5 or self.game.doesMoveEndGame(col, row+1, -self.game.turn) == None:
+                self.validMoves[self.validMoveCount] = col
+                self.validMoveCount += 1
+
+
+        # Pick a random move from this list of 'safe' moves
+        if self.validMoveCount > 0:
+            #return self.validMoves[ self.game.turnNum % self.validMoveCount ]
+            return self.validMoves[ random.randint(0,self.validMoveCount-1) ]
+
+
+        # Return a random move
+        self.validMoves.fill(0)
+        self.validMoveCount = 0
+        for i in range(7):		# Add all possible moves to valid moves list
+            if self.game.isValidMove(i):
+                self.validMoves[self.validMoveCount] = i
+                self.validMoveCount += 1
+        #return self.validMoves[ self.game.turnNum % self.validMoveCount ]
+        return self.validMoves[ random.randint(0,self.validMoveCount-1) ]
+
+
+
+
+# class botNN():
+# 	def __init__(self, game, NN):
+# 		self.game = game
+# 		self.NN = NN
+
+# 		# Used for bot to loop through each future move and give it a value
+# 		self.moveRanks = np.array([-1.]*7)
+
+# 		self.selectionNoise = False
+
+# 	def getMove(self):
+
+# 		for i in range(7):	# for all possible moves
+# 			if self.game.isValidMove(i):		# if move is valid
+# 				# gets bots rating of this move
+
+# 				# get input for NN
+# 				boardInput = self.game.getBotInput()
+
+# 				# make future move in this board input
+# 				replacementIndex = i*6
+# 				while boardInput[replacementIndex] != 0:
+# 					replacementIndex += 1
+# 				boardInput[replacementIndex] = 1
+
+# 				self.moveRanks[i] = self.NN.getOutput(boardInput)[0]
+
+# 		selectedMove = -1
+# 		if self.selectionNoise:
+# 			# Select highest rated move with certain probability
+# 			while True:
+# 				selectedMove = self.moveRanks.argmax()	# get highest rated move
+# 				if random.random() < 0.90 or self.moveRanks.max() == -1:	# pick move with 90% probability (or if all moves have been selected, select this move)
+# 					break
+# 				self.moveRanks[selectedMove] = -1
+# 		else:
+# 			selectedMove = self.moveRanks.argmax()	# get highest rated move
+        
+# 		self.moveRanks.fill(-1)		# reset move ranks
+
+# 		return selectedMove
---
+++ connect4/v3/playTrainingGames.py
@@ -0,0 +1,64 @@
+import numpy as np
+import random, sys
+import selectMove
+
+def playTrainingGames(n, game, model, opponent, discountFactor=0.9):
+    # Each move in each game adds an element to these arrays
+    saPairs = []
+    sPrimes = []
+    qValues = []    # These are discarded after network once stored in memory
+
+    # One element per game is added to this array
+    winners = []
+
+    for k in range(n):
+
+        while True:
+            if game.turn == 1:
+
+                move = game.getMove(model)
+                winner = game.move(move["move"])
+
+                # Add move to memory
+                saPairs.append(move["sa"])
+
+                # Add qValue for next move to memory
+                if game.turnNum > 1:
+                    qValues.append(
+                        move["qValue"] #* discountFactor
+                    )
+
+                    # Append the result state from the previous move
+                    sPrimes.append(move["saArray"])
+
+
+                        
+            else:
+                winner = opponent.playMove()
+
+            if winner != None:
+                if winner == 1:
+                    qValues.append(np.array([1]))
+                    sPrimes.append(1)
+                else:
+                    qValues.append(np.array([0]))
+                    sPrimes.append(0)
+                break
+
+        winners.append(winner)
+
+
+        # sys.exit()
+
+        # game.printGame()
+        # print("winner #{}: {}".format(k, winner))
+        print("Game #{}/{}".format(k+1,n), end='\r')
+
+        game.resetGame()
+
+    return {
+        "saPairs": np.array(saPairs),
+        "sPrimes": np.array(sPrimes),
+        "qValues": np.array(qValues),
+        "winners": winners
+    }
